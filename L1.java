8df8547321cfba4ae35c3683b980734c7baa6996
Rohitt <rohitingle126@gmail.com>
2025-11-10 17:24:17 +0530

    first commit

---
+++ L1.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class L1 {
+
+	public static void main(String[] args) {
+		LinkedList<String> fruits=new LinkedList();
+		fruits.add("Apple");
+		fruits.add("Banana");
+		fruits.add("Orange");
+		fruits.add("mango");
+		fruits.add("grapes");
+		System.out.println(fruits);
+	}
+
+}
