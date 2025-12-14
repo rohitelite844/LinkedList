43518c6878130d9b8654efc72c9484a99a39aa75
Rohit Ingle <rohitingle126@gmail.com>
2025-12-14 12:24:56 +0530

    Add files via upload

---
+++ LL16.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class LL16 {
+
+	public static void main(String[] args) {
+		LinkedList<String> ll=new LinkedList<>();
+		
+		ll.add("Red");
+		ll.add("Green");
+		ll.add("Black");
+		ll.add("Pink");
+		ll.add("Orange");
+		
+		System.out.println("Linked list before shuffling :"+ll);
+		
+		System.out.println();
+		
+		System.out.println("Linked list after shuffling :");
+		
+		
+
+	}
+
+}
