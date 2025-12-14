9f8f5d40b07a6ad45431b5fc7fbd68bd4795e0ee
Rohit Ingle <rohitingle126@gmail.com>
2025-12-14 12:21:27 +0530

    Add files via upload

---
+++ LL15.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class LL15 {
+
+	public static void main(String[] args) {
+		ArrayList<String> ll=new ArrayList<>();
+		ll.add("Red");
+		ll.add("Green");
+		ll.add("Black");
+		ll.add("Pink");
+		ll.add("Orange");
+		
+		System.out.println("The Original Linked List :"+ll);
+		
+		System.out.println("The new Linked list after swap :");
+		
+	}
+
+}
